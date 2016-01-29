119ab4690ba1cec24355cc730af5bd0d3e447657
mpearmain <michael.pearmain@gmail.com>
2016-01-29 11:48:00 +0000

    Adding pmml and test case

---
+++ example_isotonicPMML.py
@@ -0,0 +1,21 @@
+
+import isotonic2pmml
+import numpy as np
+from sklearn.isotonic import IsotonicRegression
+from sklearn.utils import check_random_state
+
+n = 100
+x = np.arange(n)
+rs = check_random_state(0)
+y = rs.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
+
+###############################################################################
+# Fit IsotonicRegression
+ir = IsotonicRegression()
+y_ = ir.fit_transform(x, y)
+
+# Export to PMML.
+isotonic2pmml.topmml(isotonic = ir,
+                     datafield_name = 'x',
+                     targetfield_name = 'y',
+                     outputfile = 'test.pmml')
